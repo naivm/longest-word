1c60ad2d27705d6c85630033dd1ddc15903f8899
miguel vian <miguel.vian@live.com.pt>
2024-06-04 19:11:44 +0000

    can't add dictionary using my method

--- longest_word/game.py
+++ longest_word/game.py
@@ -2,12 +2,19 @@
 # pylint: disable=too-few-public-methods
 import string
 import random
+import requests
 
 class Game:
     def __init__(self) -> list:
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found'] == json_response['found']
 
     def is_valid(self, word: str) -> bool:
         if word is None:
