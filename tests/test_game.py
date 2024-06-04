cc72c9fd022cbfcd3400783527e949547cdcc71e
miguel vian <miguel.vian@live.com.pt>
2024-06-04 18:55:49 +0000

    fixing stuff

--- tests/test_game.py
+++ tests/test_game.py
@@ -31,3 +31,9 @@ class TestGame:
         word = "GOLEM"
         new_game.grid = list(grid)
         assert new_game.is_valid(word) is False
+
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the English dictionary should not be valid"""
+        new_game = Game()
+        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+        assert new_game.is_valid('FEUN') is False
